f9ae204adde91c6c67aeebc3fc9281154bbec5e1
Ritchie Turner <ritchie@async.cl>
2013-04-29 15:59:51 -0400

    allow mixed refs (implicit, by name)

--- acl/J.hx
+++ acl/J.hx
@@ -12,6 +12,11 @@ import JQuery;
 
 typedef TJq = JQuery;
 
+enum EMixedRefs {
+	S(selector:String); //self named
+	N(name:String,selector:String); // explicitly named
+}
+
 class J {
 	
 	public static var _= JQuery._static;
@@ -30,6 +35,13 @@ class J {
 		new JQuery(fn);
 	}
 	
+	static function validRef(ref) {
+		var valid = ref.length > 0;
+		if (!valid)
+			Core.err(ref+" does not exist on page");
+		return valid;
+	}
+	
 	/**
 		Make an object with names from the given array and populate it with TJq instances
 		of the corresponding IDs from the page.
@@ -37,10 +49,24 @@ class J {
 	public static function makeRefs(refs:Array<String>):Dynamic {
 		return refs.foldLeft({},function(acc,el) {
 			var ref = J.q(el);
-			if (ref.length == 0) {
-				Core.err(el+" does not exist on page");
-			} else {
+			if (validRef(ref))
 				Reflect.setField(acc,el.substr(1),ref);
+			return acc;
+		});
+	}
+	
+	public static function makeRefsMixed(refs:Array<EMixedRefs>):Dynamic {
+		return refs.foldLeft({},function(acc,el) {
+			switch(el) {
+				case S(selector):
+					var ref = J.q(selector);
+					if (validRef(ref))
+						Reflect.setField(acc,selector.substr(1),ref);
+					
+				case N(name,selector):
+					var ref = J.q(selector);
+					if (validRef(ref))
+						Reflect.setField(acc,name,ref);
 			}
 			return acc;
 		});
